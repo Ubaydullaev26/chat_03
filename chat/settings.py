0b454b26e672b8c4a1a8d1d4834e7ff7e99fe9b5
aziz <az.ubaydullaev03@gmail.com>
2024-12-05 17:58:47 +0500

    fixesss

--- chat/settings.py
+++ chat/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-l89=rz=0q&8$@25jbuy2^0-73@5184uz^li4#0a@b#di)%4mw-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
